9dec3ef3266362995bf90c49974a18921c2d07b9
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2021-09-04 18:00:05 -0300

    creacion inicial

--- src/servlet/Signin.java
+++ src/servlet/Signin.java
@@ -1,16 +1,21 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.LinkedList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import entities.Persona;
+import logic.Login;
+
 /**
  * Servlet implementation class Signin
  */
-@WebServlet({ "/Signin", "/signin", "/SIGNIN", "/SignIn" })
+@WebServlet({ "/signin", "/SIGNIN", "/Signin", "/SignIn", "/signIn" })
 public class Signin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,15 +32,33 @@ public class Signin extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("get at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Persona per = new Persona();
+		Login ctrl = new Login();
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		//validar email y password
+		
+		per.setEmail(email);
+		per.setPassword(password);
+		
+		per=ctrl.validate(per);
+		LinkedList<Persona> personas = ctrl.getAll();
+		
+		
+		request.getSession().setAttribute("usuario", per);
+		request.setAttribute("listaPersonas", personas);
+		
+		request.getRequestDispatcher("WEB-INF/UserManagement.jsp").forward(request, response);
+		
 	}
 
 }
