f6c87cdadc52059e8fde49aee20816c25fd4a1af
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2021-09-25 14:53:49 -0300

    se agregaron los db para la nueva base

--- src/data/DbConnector.java
+++ src/data/DbConnector.java
@@ -9,9 +9,9 @@ public class DbConnector {
 	private String driver="com.mysql.cj.jdbc.Driver";
 	private String host="localhost";
 	private String port="3306";
-	private String user="java";
-	private String password="himitsu";
-	private String db="java";
+	private String user="peluquerias";
+	private String password="nichufia";
+	private String db="peluquerias";
 	private int conectados=0;
 	private Connection conn=null;
 	
