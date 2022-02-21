167932cc9622e4b8a9b64b56f1a1dfef0e60c888
SofiaErrecalde <sofiaerrecalde@hotmail.com>
2022-02-20 23:14:20 -0300

    fix publication list

--- src/servletsBarber/Publication/CreatePublicationServlet.java
+++ src/servletsBarber/Publication/CreatePublicationServlet.java
@@ -57,8 +57,8 @@ public class CreatePublicationServlet extends HttpServlet {
 		
 		try {
 			String destPage = "WEB-INF/Barber/Publications/PublicationsManagement.jsp";
-			request.setAttribute("publicationsList", PublicationBarber.getByBarberId(user.getUserId()));
 			int idNewPublication = PublicationBarber.add(newPublication);
+			request.setAttribute("publicationsList", PublicationBarber.getByBarberId(user.getUserId()));			
 			if(idNewPublication != -1) {
 				String message = "La publicación se agregó correctamente";
 				request.setAttribute("successMessage", message);
