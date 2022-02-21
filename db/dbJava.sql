a0f26fcbe256b88f0829f43c53629f806364d9d3
SofiaErrecalde <sofiaerrecalde@hotmail.com>
2022-02-20 22:13:05 -0300

    update db

--- db/dbJava.sql
+++ db/dbJava.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-09-2021 a las 23:10:51
+-- Tiempo de generación: 21-02-2022 a las 02:11:27
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -20,8 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `peluquerias`
 --
-CREATE DATABASE IF NOT EXISTS `peluquerias` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `peluquerias`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `barber_local`
+--
+
+DROP TABLE IF EXISTS `barber_local`;
+CREATE TABLE `barber_local` (
+  `id` int(11) NOT NULL,
+  `barber_id` int(11) NOT NULL,
+  `local_id` int(11) NOT NULL,
+  `day_of_week` varchar(15) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -29,6 +43,7 @@ USE `peluquerias`;
 -- Estructura de tabla para la tabla `comments`
 --
 
+DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL,
   `text` text NOT NULL,
@@ -43,25 +58,29 @@ CREATE TABLE `comments` (
 -- Estructura de tabla para la tabla `locals`
 --
 
+DROP TABLE IF EXISTS `locals`;
 CREATE TABLE `locals` (
   `local_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `coordenates` text NOT NULL,
-  `address` varchar(255) NOT NULL
+  `address` varchar(255) NOT NULL,
+  `is_enable` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `posts`
+-- Estructura de tabla para la tabla `publications`
 --
 
-CREATE TABLE `posts` (
-  `post_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `publications`;
+CREATE TABLE `publications` (
+  `publication_id` int(11) NOT NULL,
   `barber_id` int(11) NOT NULL,
+  `title` text NOT NULL,
   `text` text NOT NULL,
-  `image` blob DEFAULT NULL,
-  `is_deleted` tinyint(1) DEFAULT NULL
+  `date` text NOT NULL DEFAULT current_timestamp(),
+  `image` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -70,6 +89,7 @@ CREATE TABLE `posts` (
 -- Estructura de tabla para la tabla `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
   `role` varchar(255) NOT NULL
@@ -81,6 +101,7 @@ CREATE TABLE `roles` (
 -- Estructura de tabla para la tabla `role_user`
 --
 
+DROP TABLE IF EXISTS `role_user`;
 CREATE TABLE `role_user` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL
@@ -92,13 +113,29 @@ CREATE TABLE `role_user` (
 -- Estructura de tabla para la tabla `services`
 --
 
+DROP TABLE IF EXISTS `services`;
 CREATE TABLE `services` (
   `service_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `price` decimal(10,0) NOT NULL,
-  `duration` time DEFAULT NULL
+  `duration` time DEFAULT NULL,
+  `is_enable` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `service_barber`
+--
+
+DROP TABLE IF EXISTS `service_barber`;
+CREATE TABLE `service_barber` (
+  `service_barber_id` int(11) NOT NULL,
+  `barber_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `is_enable` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -106,6 +143,7 @@ CREATE TABLE `services` (
 -- Estructura de tabla para la tabla `turns`
 --
 
+DROP TABLE IF EXISTS `turns`;
 CREATE TABLE `turns` (
   `turn_id` int(11) NOT NULL,
   `local_id` int(11) NOT NULL,
@@ -122,6 +160,7 @@ CREATE TABLE `turns` (
 -- Estructura de tabla para la tabla `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -134,19 +173,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `dni`, `phone`, `email`, `password`, `is_enable`) VALUES
-(1, 'Nicolas', 'Bologna', 39916467, NULL, 'nbologna@gmail.com', '299132688689127175738334524183350839358', 1),
-(2, 'Nicolas', 'Bologna', 134, '3413882211', 'nbolognadas@gmail.com', '123456', 1),
-(3, 'asdasdas', 'asdasd', 0, '12412354', 'cuenta2@ads.com', '123rfdsdf', 1),
-(4, 'Nicolas', 'Bologna', 134525, '3413882211', 'nbologna2@gmail.com', '123456', 1),
-(5, 'Nicolas', 'Bologna', 1343243141, '3413882211', 'nbolog13na@gmail.com', '12341314', 1);
-
---
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `barber_local`
+--
+ALTER TABLE `barber_local`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `comments`
@@ -161,10 +195,10 @@ ALTER TABLE `locals`
   ADD PRIMARY KEY (`local_id`);
 
 --
--- Indices de la tabla `posts`
+-- Indices de la tabla `publications`
 --
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`post_id`);
+ALTER TABLE `publications`
+  ADD PRIMARY KEY (`publication_id`);
 
 --
 -- Indices de la tabla `roles`
@@ -196,6 +230,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `barber_local`
+--
+ALTER TABLE `barber_local`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
@@ -208,10 +248,10 @@ ALTER TABLE `locals`
   MODIFY `local_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `posts`
+-- AUTO_INCREMENT de la tabla `publications`
 --
-ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `publications`
+  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -229,7 +269,7 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
